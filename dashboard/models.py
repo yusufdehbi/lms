c858fc704dfa803e2772645ec64233f0de91b3a9
Youssef Dehbi <youssefdehbi59@gmail.com>
2024-05-09 13:30:10 +0100

    add employee model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)
+    date_of_hire = models.DateField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
