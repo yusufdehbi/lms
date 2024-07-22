533e2a4f3ccb7d3917829c57ec0dc4953307aff5
youssef <youssefdehbi59@gmail.com>
2024-07-22 19:45:07 +0100

    fetch and display employees in employees page list

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Employee
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 def home(request):
     return render(request, 'overview.html')
+
+
+def employees(request):
+    employees = Employee.objects.all()
+    paginator = Paginator(employees, 1)
+    page_number = request.GET.get('page')
+    page_employees = paginator.get_page(page_number)
+    return render(request, 'employees.html', {'employees': page_employees})
