c14d9aed45e334439a46999699fd5db70217c84e
youssef <youssefdehbi59@gmail.com>
2024-08-18 17:10:10 +0100

    recreate add employee page rendered from the django form of user and employee

--- dashboard/forms/employee_forms.py
+++ dashboard/forms/employee_forms.py
@@ -6,3 +6,23 @@ class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = ['employee_id', 'department', 'position', 'date_joined']
+        widgets = {
+            'employee_id': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'employee id'
+            }),
+            'department': forms.Select(attrs={
+                'class': 'js-select2 form-select',
+                'data-placeholder': 'Choose department'
+            }),
+            'position': forms.Select(attrs={
+                'class': 'js-select2 form-select',
+                'data-placeholder': 'Choose position'
+            }),
+            'date_joined': forms.DateInput(attrs={
+                'class': 'js-flatpickr form-control',
+                'placeholder': 'Y-m-d'
+            }),
+        }
+
+
--- dashboard/forms/user_forms.py
+++ dashboard/forms/user_forms.py
@@ -3,6 +3,37 @@ from dashboard.models import User
 
 
 class UserForm(forms.ModelForm):
+    gender = forms.ChoiceField(
+        choices=User.GENDER_CHOICES,
+        widget=forms.RadioSelect(attrs={
+            'class': 'form-check-input'
+        }),
+        required=True,
+        initial=User.MALE
+    )
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'birthday', 'gender']
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'first name'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'last name'
+            }),
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'username'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'email'
+            }),
+            'birthday': forms.DateInput(attrs={
+                'class': 'js-flatpickr form-control',
+                'placeholder': 'Y-m-d'
+            }),
+        }
