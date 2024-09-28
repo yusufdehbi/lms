98b1cba8dd13cb750d5c9676592ee4edba1289cb
youssefautomize <youssef.dehbi@automize.ma>
2024-09-28 22:33:25 +0100

    remove a uncessery retrun

--- dashboard/utils/auth_utils.py
+++ dashboard/utils/auth_utils.py
@@ -11,7 +11,6 @@ def role_required(allowed_roles=None):
         def _wrapped_view(request, *args, **kwargs):
             if request.user.is_authenticated and request.user.role in allowed_roles:
                 return view_func(request, *args, **kwargs)
-            return
             return HttpResponseForbidden("You do not have permission to access this page.")
 
         return _wrapped_view
